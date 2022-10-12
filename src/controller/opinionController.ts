a1061969447c6db8ef1f8e4d7008c4949d1bd8be
Alvaro5555 <91837567+Alvaro5555@users.noreply.github.com>
2022-10-12 15:48:47 +0200

    Update opinionController.ts

--- src/controller/opinionController.ts
+++ src/controller/opinionController.ts
@@ -1,14 +1,16 @@
-import User from "src/model/User";
-import Opinion from "src/model/Opinion";
-import Parking from "src/model/Parking";
+import User from '../model/User';
+import Opinion from "../model/Opinion";
+import Parking from "../model/Parking";
 import { Request, Response } from 'express';
 
-const Opinion = async (req: Request, res: Response) => {
+const opinion = async (req: Request, res: Response) => {
 	const { user, parking, date, description, puntuacio } = req.body;
     const customer = await User.findOne({ name: user });
+    
     const parkingopin = await Parking.findById(parking);
+   
     const newOpinion = new Opinion({
-		user: customer._id
+		user: customer._id,
         parking: parkingopin._id,
 		date: date,
 		description: description,
@@ -17,4 +19,16 @@ const Opinion = async (req: Request, res: Response) => {
 await newOpinion.save();
 res.status(200).json({ auth: true });
 };
-
+const getallOpinionsPark = async (req: Request, res: Response) => {
+	const opinions = await Opinion.findById(req.params.parking._id);
+	res.json(opinions);
+};
+const getallOpinionsUser = async (req: Request, res: Response) => {
+	const opinions = await Opinion.findById(req.params.user._id);
+	res.json(opinions);
+};
+export default {
+	opinion,
+	getallOpinionsPark,
+    getallOpinionsUser
+};
