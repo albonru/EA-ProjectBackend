d8815b7a5d1444edf4465bea2f286702eff122e9
Aida Padilla <aida.padilla.gonzalez.aiko@gmail.com>
2022-10-29 19:05:15 +0200

    afegir llista

--- src/model/Parking.ts
+++ src/model/Parking.ts
@@ -11,6 +11,11 @@ const Parking = new Schema({
         type: Schema.Types.ObjectId,
 		ref: "Opinion"
     },
+    country: String,
+	city: String,
+	street: String,
+	spotNumber: Number,
+    
     type: String,
     price: Number,
     size: String,
