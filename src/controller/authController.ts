21840cc6c29c0fb52c8731c89401823f60299d8a
Aida Padilla <aida.padilla.gonzalez.aiko@gmail.com>
2022-12-10 23:14:50 +0100

    intent tslint

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -12,7 +12,7 @@ export async function login (req: Request, res: Response): Promise<Response> {
 try {
     const userFound = await User.findOne({ email });
     if (!userFound) return res.status(400).json({ message: "User Not Found ", userFound, email, password });
-    if (userFound.deleted) return res.status(401).json({ message: "Deactivated User" })
+    if (userFound.deleted) return res.status(402).json({ message: "Deactivated User" })
     const validPassword = CryptoJS.AES.decrypt(userFound.password as string, 'secret key 123').toString(CryptoJS.enc.Utf8);
     const index = validPassword.localeCompare(password);
     if (index === 0) {
@@ -24,7 +24,7 @@ try {
         const id = userFound.id;
         return res.json({ auth: true, token, id });
     }
-    return res.status(401).json({ auth: false, token: null });
+    return res.status(402).json({ auth: false, token: null });
     }
     catch(err) {
         res.status(404).send('Cant find user');
